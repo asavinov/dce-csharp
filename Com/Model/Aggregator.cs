9c1083aafcf7ec2d35f4ec8c82a9af618cb6eb49
Alexandr Savinov <savinov@conceptoriented.org>
2013-07-14 22:42:52 +0200

    Array expansion methods added as alternative.
    Former-commit-id: c141fced85c80e5a040522d70ec05938130ee416

--- Com/Model/Aggregator.cs
+++ Com/Model/Aggregator.cs
@@ -17,39 +17,85 @@ namespace Com.Model
         T Div(T a, int count); // надо уметь делить на целое, т.к. длина массива - целая величина
     }
 
-    // We implement aggregation functions separately for each primitive type
+    public static class ComArray
+    {
+        public static object Sum(this System.Collections.IEnumerable values)
+        {
+            return values.Sum(); // System method of Array
+        }
+        public static object Avg(this System.Collections.IEnumerable values)
+        {
+             var intArray = values as int[];
+             if (intArray != null)
+             {
+                 return intArray.Average();
+             }
+             var doubleArray = values as double[]; 
+             if (doubleArray != null)
+             {
+                 return doubleArray.Average();
+             }
+            // And so on for all primitive types
+             return null;
+        }
+    }
+
+    /// <summary>
+    /// Aggregation functions for int[]
+    /// </summary>
     public struct IntAggregator : IAggregator<int>
     {
         public int Sum(int[] values) 
         {
-            int agg = 0;
-            for (int i = 0; i < values.Length; i++) agg += values[i];
-            return agg; 
+            return values.Sum(); 
         }
         public int Avg(int[] values)
         {
-            return Sum(values) / values.Length;
+            return (int)values.Average();
         }
 
         public int Add(int a, int b) { return a + b; }
         public int Div(int a, int b) { return a / b; }
     }
+    public static class IntArray // Expand int[]
+    {
+        public static int Sum(this IEnumerable<int> values)
+        {
+            return values.Sum();
+        }
+        public static int Avg(this IEnumerable<int> values)
+       {
+            return (int)values.Average();
+        }
+    }
 
+    /// <summary>
+    /// Aggregation functions for double[]
+    /// </summary>
     public struct DoubleAggregator : IAggregator<double>
     {
         public double Sum(double[] values)
         {
-            double agg = 0;
-            for (int i = 0; i < values.Length; i++) agg += values[i];
-            return agg;
+            return values.Sum();
         }
         public double Avg(double[] values)
         {
-            return Sum(values) / values.Length;
+            return values.Average();
         }
 
         public double Add(double a, double b) { return a + b; }
         public double Div(double a, int b) { return a / b; }
+    }
+    public static class DoubleArray // Expand double[]
+    {
+        public static double Sum(this IEnumerable<double> values)
+        {
+            return values.Sum();
+        }
+        public static double Avg(this IEnumerable<double> values)
+        {
+            return values.Average();
+        }
     }
 
 }
