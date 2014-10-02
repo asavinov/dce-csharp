cba73dc61a4b6b040c38712d7b672e535a109d62
Alexandr Savinov <savinov@conceptoriented.org>
2014-10-02 12:31:16 +0200

    Change of assembly properties and project description
    Former-commit-id: 3213aa17d1fe54b56ea3ed9504115a5a2f213e1e

--- Com/Properties/AssemblyInfo.cs
+++ Com/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Com")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("DataCommander: Integrate. Transform. Analyze")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Alexandr Savinov")]
 [assembly: AssemblyProduct("Com")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © 2013-2014 Alexandr Savinov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.2")]
+[assembly: AssemblyFileVersion("0.2")]
